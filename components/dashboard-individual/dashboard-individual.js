e7dcd7a3e0ccf81e718e22c47f2e346b00b1cb03
Matthew Wu <mnfwu12@gmail.com>
2021-07-27 14:10:34 +0800

    change app header, init dashboard-individual component

---
+++ components/dashboard-individual/dashboard-individual.js
@@ -0,0 +1,23 @@
+// components/dashboard-individual/dashboard-individual.js
+Component({
+  /**
+   * Component properties
+   */
+  properties: {
+
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+
+  }
+})
