c1488051c433677533b55cf128770fbeb9b39168
qixi z <zqx0803@me.com>
2021-07-27 13:02:35 +0800

    form draft

--- components/button/button.js
+++ components/button/button.js
@@ -3,6 +3,8 @@ Component({
   /**
    * 组件的属性列表
    */
+  behaviors: ['wx://form-field-button'],
+
   properties: {
     text: String,
     color: String
--- components/task-form/task-form.js
+++ components/task-form/task-form.js
@@ -18,6 +18,8 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    submit: function (e) {
+      console.log("表单携带的数据：", e.detail.value)
+    },
   }
 })
