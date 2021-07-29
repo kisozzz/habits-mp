34d050a703f57d80ec003e73733a6807cdbf8607
Matthew Wu <mnfwu12@gmail.com>
2021-07-29 11:46:15 +0800

    add task-card put function

--- components/task-card/task-card.js
+++ components/task-card/task-card.js
@@ -19,11 +19,31 @@ Component({
    */
   methods: {
     checkDone() {
-      // console.log(this.data)
-      if(this.data.doneTap == true){
+      const stepId = this.data.info.id
+      if(this.data.doneTap == true) {
         this.setData({ doneTap: false })
-      }else{
+        // SEND PUT REQUEST
+        wx.request({
+        // old url: `https://habits.wogengapp.cn/api/v1/master_habits/:master_habit_id/habits/:habit_id/steps/:id`,
+        url: `https://habits.wogengapp.cn/api/v1/steps/${stepId}`,
+        method: 'PUT',
+        data: { completed: false },
+        success(res) {
+            console.log(res)
+           }
+         })
+        // console.log(this.data.doneTap)
+      } else {
         this.setData({ doneTap: true })
+        wx.request({
+        // old url: `https://habits.wogengapp.cn/api/v1/master_habits/:master_habit_id/habits/:habit_id/steps/:id`,
+          url: `https://habits.wogengapp.cn/api/v1/steps/${stepId}`,
+          method: 'PUT',
+          data: { completed: true },
+          success(res) {
+            console.log(res)
+           }
+         })
       }  
     },
   }
