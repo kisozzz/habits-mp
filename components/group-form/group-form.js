504d9f2b856bbf51b0f3f9fbef2c75a2cc4befe5
qixi z <zqx0803@me.com>
2021-07-30 14:39:28 +0800

    delete unused ui

--- components/group-form/group-form.js
+++ components/group-form/group-form.js
@@ -20,15 +20,14 @@ Component({
   methods: {
     submit: function (e) {
       console.log('form发生了submit事件，携带数据为：', e.detail.value)
-      console.log(this.data)
-      console.log(999,getApp().globalData.user)
-      const user_id = getApp().globalData.user.id;
+      // console.log(this.data)
+      // console.log(999,getApp().globalData.user)
+      // const user_id = getApp().globalData.user.id;
       const name = e.detail.value.name;
       let group = {
-        user_id: user_id,
+        // user_id: user_id,
         name: name
       }
-      console.log(group)
       wx.request({
         url: "https://habits.wogengapp.cn/api/v1/groups",
         method: 'POST',
