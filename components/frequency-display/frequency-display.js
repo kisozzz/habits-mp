5ed9571f841da9e89b79f71ab5503705680e012d
Matthew Wu <mnfwu12@gmail.com>
2021-07-30 15:35:10 +0800

    saving dashboard progress

--- components/frequency-display/frequency-display.js
+++ components/frequency-display/frequency-display.js
@@ -1,8 +1,28 @@
 // components/frequency-display/frequency-display.js
 Component({
-  /**
-   * Component properties
-   */
+  lifetimes: {
+    ready: function() {
+      console.log('freq db', this.data)
+      // const otherHabits = this.data.info.other_habits
+      let completedStatuses = []
+    }
+  },
+  // let completedStatuses = []
+  // otherHabits.forEach(habit => {
+  //   if (habit.completed) {
+  //     completedStatuses.push('complete')
+  //   } else if (habit.partially_completed) {
+  //     completedStatuses.push('partial')
+  //   } else {
+  //     completedStatuses.push('incomplete')
+  //   }
+  // })
+  // page.setData({
+  //   habit: habit,
+  //   completedStatuses: completedStatuses
+  // });
+
+
   properties: {
     info: Object
   },
