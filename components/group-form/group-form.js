473b87c1b9afeb2e72e7554715d35f2aadd7260f
qixi z <zqx0803@me.com>
2021-07-30 15:02:00 +0800

    post group

--- components/group-form/group-form.js
+++ components/group-form/group-form.js
@@ -20,12 +20,12 @@ Component({
   methods: {
     submit: function (e) {
       console.log('form发生了submit事件，携带数据为：', e.detail.value)
-      // console.log(this.data)
-      // console.log(999,getApp().globalData.user)
-      // const user_id = getApp().globalData.user.id;
+      console.log(this.data)
+      console.log(999,getApp().globalData.user)
+      const user_id = getApp().globalData.user.id;
       const name = e.detail.value.name;
       let group = {
-        // user_id: user_id,
+        user_id: user_id,
         name: name
       }
       wx.request({
