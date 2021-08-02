7b103d24894c7b9d9c93d6797adf6b1005d9abd7
qixi z <zqx0803@me.com>
2021-08-02 11:36:39 +0800

    group-card-layot

---
+++ components/group-card/group-card.js
@@ -0,0 +1,25 @@
+import string from "../../miniprogram_npm/lin-ui/common/async-validator/validator/string";
+
+// components/group-card/group-card.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    title: String,
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
