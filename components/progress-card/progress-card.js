6d5a0828a3f4bd27233e32310b6b95063604c190
Matthew Wu <mnfwu12@gmail.com>
2021-08-02 11:56:46 +0800

    add analysis fetch routes to progress-cards

--- components/progress-card/progress-card.js
+++ components/progress-card/progress-card.js
@@ -14,6 +14,24 @@ Component({
 
   },
 
+  attached: function(){
+    console.log(this.data.info.id)
+    const masterHabitId = this.data.info.id
+    const that = this;
+    wx.request({
+      url: `https://habits.wogengapp.cn/api/v1/master_habits/${masterHabitId}/analytics`,
+      method: "GET",
+      success(res) {
+        const progress = res.data;
+        console.log(progress)
+        that.setData({
+          progress: progress
+        });
+      } 
+      }) 
+  },
+  // analytics route: https://habits.wogengapp.cn/api/v1/master_habits/36/analytics
+
   /**
    * Component methods
    */
