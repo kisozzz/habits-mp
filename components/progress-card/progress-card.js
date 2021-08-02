65fa75c9fc6a7e33f09140a8c024d6f91cea46ab
Matthew Wu <mnfwu12@gmail.com>
2021-08-02 10:35:47 +0800

    init progress-card component

---
+++ components/progress-card/progress-card.js
@@ -0,0 +1,23 @@
+// components/progress-card/progress-card.js
+Component({
+  /**
+   * Component properties
+   */
+  properties: {
+
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+
+  }
+})
